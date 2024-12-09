dcca8305044a9468a5e40ab6d001bdef17e95afe
shurajan <shurajangit@gmail.com>
2024-12-09 21:00:35 +0300

    Added openapi dependencies

--- TravelSchedule/ContentView.swift
+++ TravelSchedule/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import OpenAPIURLSession
 
 struct ContentView: View {
     var body: some View {
@@ -16,6 +17,30 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .onAppear {
+            do {
+                try stations()
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    func stations() throws {
+        let client = Client(
+            serverURL: try Servers.Server1.url(),
+            transport: URLSessionTransport()
+        )
+        
+        let service = NearestStationsService(
+            client: client,
+            apikey: "05d03cb7-d988-4bb4-92cb-513b293fd349"
+        )
+        
+        Task {
+            let stations = try await service.getNearestStations(lat: 59.864177, lng: 30.319163, distance: 50)
+            print(stations)
+        }
     }
 }
 
